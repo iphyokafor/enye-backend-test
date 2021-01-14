a3ddfd1ad5e865d9501ec7761f07584b7a467abc
iphyokafor <sandraifeoma22@gmail.com>
2021-01-14 22:53:22 +0100

    modified exchangeRateRouter to fetchAllExchangeRates without query params

--- src/routes/exchangeRate.js
+++ src/routes/exchangeRate.js
@@ -3,31 +3,37 @@ const router = express.Router();
 const fetch = require("node-fetch");
 
 router.get("/", async(req, res) => {
-    let { base, currency } = req.query;
     const url = "https://api.exchangeratesapi.io/latest";
     try {
-        const fetchExchangeRates = await fetch(
-            `${url}?base=${base}&currency=${currency}`
-        );
+        const fetchAllExchangeRates = await fetch(`${url}`);
+        const results = await fetchAllExchangeRates.json();
 
-        const result = await fetchExchangeRates.json();
-        const rates = {};
-        currency.split(",").map((item) => {
-            if (!result.rates[item]) {
-                return res.status(400).json({
-                    message: "Please Check that all currencies entered are correct",
-                });
-            }
-            return (rates[item] = result.rates[item]);
-        });
+        if (req.query) {
+            let { base, currency } = req.query;
+            const fetchExchangeRates = await fetch(
+                `${url}?base=${base}&currency=${currency}`
+            );
 
-        return res.status(200).json({
-            results: {
-                base: result.base,
-                date: result.date,
-                rates,
-            },
-        });
+            const result = await fetchExchangeRates.json();
+            const rates = {};
+            currency.split(",").map((item) => {
+                if (!result.rates[item]) {
+                    return res.status(400).json({
+                        message: "Please Check that all currencies entered are correct",
+                    });
+                }
+                return (rates[item] = result.rates[item]);
+            });
+
+            return res.status(200).json({
+                results: {
+                    base: result.base,
+                    date: result.date,
+                    rates,
+                },
+            });
+        }
+        return res.status(200).json(results);
     } catch (error) {
         res.status(500).json(error.message);
     }
